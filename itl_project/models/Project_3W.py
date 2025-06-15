14930a212fb2c5ddbb1fa327b5f4981e3d2f7ca4
itl-selim <selim.reza@itl-group.com.bd>
2025-06-15 12:28:32 +0600

    Add project.three.w model class

---
+++ itl_project/models/Project_3W.py
@@ -0,0 +1,14 @@
+from odoo import fields, models
+
+class Project3W(models.Model):
+    _name = "project.three.w"
+    _description = "Project 3W"
+
+    name = fields.Char("Action Items")
+    day_date = fields.Date("Date")
+    when_date = fields.Date("When")
+    responsible = fields.Many2many("res.partner", string="Responsible")
+    status = fields.Selection([
+        ('not_started', 'Not started'),
+        ('in_progress', 'In progress'),
+        ('completed', 'Completed')], default='not_started', string="Status")
--- itl_project/models/__init__.py
+++ itl_project/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import project_project, ProjectKpi
+from . import project_project, ProjectKpi, Project_3W
