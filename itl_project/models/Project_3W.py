f7ff0528bd7095c8bcebdd11e05598bcc850b6bc
itl-selim <selim.reza@itl-group.com.bd>
2025-06-26 14:23:00 +0600

    Make all fields of 3W project readonly for normal users

--- itl_project/models/Project_3W.py
+++ itl_project/models/Project_3W.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class Project3W(models.Model):
     _name = "project.three.w"
@@ -13,3 +13,19 @@ class Project3W(models.Model):
         ('not_started', 'Not started'),
         ('in_progress', 'In progress'),
         ('completed', 'Completed')], default='not_started', string="Status")
+
+    # Block is been using for track who can edit
+    can_edit_fields = fields.Boolean(compute='_compute_can_edit_fields', store=False)
+
+    """This method is ensuring project module's Administrator, project leader and project coordinator can only edit certain field."""
+    @api.depends('project_id')
+    def _compute_can_edit_fields(self):
+        for task in self:
+            user = self.env.user
+            project = task.project_id
+            task.can_edit_fields = bool(project) and (
+                    user.has_group('project.group_project_manager') or
+                    (project.user_id and project.user_id.id == user.id) or
+                    (project.project_coordinator and project.project_coordinator.id == user.id)
+            )
+    # Block is been using for track who can edit
